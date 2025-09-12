7078748eec3211aa36028bed665f429cb2b7cde7
Rahil Shah <rshah10@bu.edu>
2025-09-12 16:44:21 -0400

    Updated path for CSS

--- cs412/settings.py
+++ cs412/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/rshah10/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
