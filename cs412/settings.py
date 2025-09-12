487591a8382bcc63d56a8f91067b74159570614f
Rahil Shah <rshah10@bu.edu>
2025-09-12 17:13:36 -0400

    Updated path for CSS

--- cs412/settings.py
+++ cs412/settings.py
@@ -134,7 +134,7 @@ import socket
 CS_DEPLOYMENT_HOSTNAME = 'cs-webapps.bu.edu'
 
 if socket.gethostname() == CS_DEPLOYMENT_HOSTNAME:
-    #STATIC_URL = '/rshah10/static/'
+    STATIC_URL = '/rshah10/static/'
     MEDIA_URL = '/rshah10/media/'
 
 # Default primary key field type
